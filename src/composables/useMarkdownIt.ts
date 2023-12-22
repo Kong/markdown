95a53af73f53214a42513deacaa800d815c261ae
Adam DeHaven <adam.dehaven@konghq.com>
2023-12-22 14:14:59 -0500

    refactor: header link

--- src/composables/useMarkdownIt.ts
+++ src/composables/useMarkdownIt.ts
@@ -1,6 +1,6 @@
 import { ref } from 'vue'
 import useShikiji from '@/composables/useShikiji'
-import { NEW_LINE_CHARACTER } from '@/constants'
+import { NEW_LINE_CHARACTER, HEADER_LINK_SVG } from '@/constants'
 
 // markdown-it
 import MarkdownIt from 'markdown-it'
@@ -44,7 +44,7 @@ export default function useMarkdownIt(theme: 'light' | 'dark' = 'light') {
           placement: 'before',
           class: 'header-anchor', // The class applied to the anchor tag; allows for styling
           // Utilize an SVG icon instead of a `#` string
-          symbol: '<svg class="header-anchor-icon" viewBox="0 0 16 16" version="1.1" width="14" height="14" aria-hidden="true"><path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg>',
+          symbol: HEADER_LINK_SVG,
         }),
       })
       .use(abbreviation)
