f4089da7fc0e190a8068e2f833fe16f7bbef472b
Adam DeHaven <adam.dehaven@konghq.com>
2024-03-04 20:35:03 -0500

    docs: update example

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
     minify: true,
     sourcemap: true,
     rollupOptions: {
-      external: process.env.USE_SANDBOX ? ['#mdc-highlighter'] : ['vue', '#mdc-highlighter'],
+      external: process.env.USE_SANDBOX ? undefined : ['vue'],
       output: process.env.USE_SANDBOX
         ? undefined
         : {
