73572a0a9791de350db8dc1c2df3fb3107f19d14
Adam DeHaven <adam.dehaven@konghq.com>
2024-04-08 14:02:22 -0400

    refactor: init

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,12 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// Monaco editor
-import * as monaco from 'monaco-editor'
-import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
 // Global MDC components
 import MdcAlert from './components/mdc/MdcAlert.vue'
@@ -15,27 +8,6 @@ import MdcFlex from './components/mdc/MdcFlex.vue'
 import MdcGrid from './components/mdc/MdcGrid.vue'
 import MdcGridItem from './components/mdc/MdcGridItem.vue'
 import MdcTabs from './components/mdc/MdcTabs.vue'
-
-// @ts-ignore
-self.MonacoEnvironment = {
-  getWorker(_: any, label: string) {
-    if (label === 'json') {
-      return new JsonWorker()
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new CssWorker()
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new HtmlWorker()
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new TsWorker()
-    }
-    return new EditorWorker()
-  },
-}
-
-monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true)
 
 const app = createApp(App)
 
--- src/components/MarkdownUi.vue
+++ src/components/MarkdownUi.vue
@@ -177,6 +177,11 @@ import MermaidJs from 'mermaid'
 import type { MarkdownItEnv } from '@mdit-vue/types'
 // Monaco Editor
 import * as monaco from 'monaco-editor'
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 import { language as mdcLanguage } from '../mdc.tmLanguage'
 
 const props = defineProps({
@@ -550,6 +555,27 @@ const initMonacoEditor = (): void => {
     monacoEditor.dispose()
   }
 
+  // @ts-ignore
+  self.MonacoEnvironment = {
+    getWorker(_: any, label: string) {
+      if (label === 'json') {
+        return new JsonWorker()
+      }
+      if (label === 'css' || label === 'scss' || label === 'less') {
+        return new CssWorker()
+      }
+      if (label === 'html' || label === 'handlebars' || label === 'razor') {
+        return new HtmlWorker()
+      }
+      if (label === 'typescript' || label === 'javascript') {
+        return new TsWorker()
+      }
+      return new EditorWorker()
+    },
+  }
+
+  monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true)
+
   // Create Monaco editor
   monacoEditor = monaco.editor.create(document.getElementById(textareaId.value)!, {
     value: rawMarkdown.value,
@@ -557,7 +583,7 @@ const initMonacoEditor = (): void => {
     theme: 'vs-dark', // 'vs' (default), 'vs-dark', 'hc-black'
     tabSize: 2,
     wordWrap: 'on',
-    insertSpaces: true,
+    insertSpaces: true, // insert spaces when pressing Tab
     autoClosingQuotes: 'always',
     detectIndentation: false,
     renderWhitespace: 'boundary',
@@ -566,6 +592,7 @@ const initMonacoEditor = (): void => {
     glyphMargin: false,
     lineNumbersMinChars: 3,
     overviewRulerLanes: 0,
+    automaticLayout: true,
     minimap: {
       enabled: false,
     },
@@ -592,7 +619,6 @@ const initMonacoEditor = (): void => {
       },
     }
     onContentEdit(event)
-    console.log('changed', event)
   })
 }
 
