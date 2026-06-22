db42b9ac133f7a5195f3afda81571f0c8762b1c3
Adam DeHaven <adam.dehaven@konghq.com>
2026-06-22 11:18:03 -0400

    fix: versions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,5 @@
 import eslintKongUiConfig from '@kong/eslint-config-kong-ui'
-import designTokens from '@kong/eslint-plugin-design-tokens'
 
 export default [
   ...eslintKongUiConfig,
-  {
-    files: ['**/*.vue'],
-    plugins: {
-      '@kong/eslint-plugin-design-tokens': designTokens,
-    },
-    rules: {
-      '@kong/eslint-plugin-design-tokens/token-constant-requires-css-var': 'error',
-    },
-  },
 ]
